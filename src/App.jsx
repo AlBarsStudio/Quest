6d18115f03d217d0b769b3992f2f5c5db6c99c59
AlBars <103575531+AlBarsStudio@users.noreply.github.com>
2026-08-08 18:50:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import Intro from './features/intro/Intro';
 import Dashboard from './features/dashboard/Dashboard';
+import CoreDefense from './features/quests/core_defense/CoreDefense';
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
+  // 'intro', 'dashboard', 'core_defense'
+  const [currentScreen, setCurrentScreen] = useState('intro');
 
   return (
     <>
-      {showIntro ? (
-        <Intro onComplete={() => setShowIntro(false)} />
-      ) : (
-        <Dashboard />
+      {currentScreen === 'intro' && (
+        <Intro onComplete={() => setCurrentScreen('dashboard')} />
+      )}
+      
+      {currentScreen === 'dashboard' && (
+        <Dashboard onStartQuest={(questId) => setCurrentScreen(questId)} />
+      )}
+
+      {currentScreen === 'core_defense' && (
+        <CoreDefense onReturnToDashboard={() => setCurrentScreen('dashboard')} />
       )}
     </>
   );
