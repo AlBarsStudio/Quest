3688b8e28d39155fd38a5759bf8a51babcc15bed
AlBars <103575531+AlBarsStudio@users.noreply.github.com>
2026-08-08 19:00:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import Intro from './features/intro/Intro';
 import Dashboard from './features/dashboard/Dashboard';
+// Подключаем наш новый квест:
 import CoreDefense from './features/quests/core_defense/CoreDefense';
 
 function App() {
-  // 'intro', 'dashboard', 'core_defense'
+  // Теперь стейт хранит название текущего экрана, а не просто true/false
   const [currentScreen, setCurrentScreen] = useState('intro');
 
   return (
@@ -14,6 +15,7 @@ function App() {
       )}
       
       {currentScreen === 'dashboard' && (
+        // Здесь мы передаем пропс onStartQuest, который Дашборд вызывает при клике на кнопку
         <Dashboard onStartQuest={(questId) => setCurrentScreen(questId)} />
       )}
 
